fd760f9f967edf656c14f6c390c6a02954a12b5e
Rohxn0802 <rohanshikhare08@gmail.com>
2024-04-10 10:06:13 +0530

    Frontend uploDED On git

---
+++ assets/js/main.js
@@ -0,0 +1,111 @@
+window.addEventListener("scroll", () => {
+  myFunction();
+});
+
+const handleScroll = () => {
+  const currentScrollPos = window.pageYOffset;
+  const navbar = document.getElementById("headerSection");
+
+  if (prevScrollPos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else if (prevScrollPos > 500) {
+    navbar.style.top = "-100px";
+  }
+  prevScrollPos = currentScrollPos;
+};
+
+window.addEventListener("scroll", handleScroll);
+
+function myFunction() {
+  const navbar = document.getElementById("headerSection");
+  
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+function addHeightToHeader() {
+  var headerSticky = document.querySelector('.header');
+  var body = document.querySelector('body');
+  headerSticky.classList.toggle("addHeight");
+  body.classList.toggle("overFlowClass");
+}
+
+var prevScrollPos = window.pageYOffset;
+// Initialize Slick Slider
+$(document).ready(function () {
+  $(".servicesSlider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 835,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $(".planSlider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 835,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  //accordian start
+  $('.commonAccordian').click(function(){
+    $(this).toggleClass('active');
+    $(this).find('.accordianBtn .accordianArrow').toggleClass('active');
+    var accordianContent = $(this).find('.accordianContent');
+    if(accordianContent.height() > 0) {
+      accordianContent.css('height', '0px');
+      $(this).find('.accordianBtn').removeClass('open');
+    } else {
+      var autoHeight = accordianContent.prop('scrollHeight') + 'px';
+      accordianContent.css('height', autoHeight);
+      $(this).find('.accordianBtn').addClass('open');
+    }
+  });
+  
+  
+  //accordian end
+
+});
+//slick slider ended
+
+
+
